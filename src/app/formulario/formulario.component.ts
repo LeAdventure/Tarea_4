012f9992d3feb9fe39fd219d90eab16f8107320c
Sebastian Terrazas <sebastian.terrazas@hotmail.com>
2020-02-26 17:58:17 -0600

    Final

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -8,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.scss'],
 })
 export class FormularioComponent implements OnInit {
+  
   nom: string;
+  ape: string;
+  mail: string;
+  calendar: string;
+  gender: string;
+  contra: string;
+  conf: string;
   constructor(){
     
   }
   ngOnInit() {}
+
+  isValid(): boolean{
+    if((this.nom==undefined || this.nom==' ') || (this.ape==undefined || this.ape==' ') || (this.mail==undefined || this.mail==' ') || (this.mail==undefined) || (this.gender==undefined) || (this.contra==undefined || this.contra==' ') || ((this.conf==undefined || this.conf==' ') || this.conf!=this.contra)){
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
 
 }
